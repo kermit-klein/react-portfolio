8a0b70d721e4fb506e51cc82d80309ddb14a58ce
allezz <aerbay@gmail.com>
2020-04-15 23:11:38 +0200

    Add Undraw images,netlify redirect file

--- src/About.jsx
+++ src/About.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { UndrawArtificialIntelligence } from "react-undraw-illustrations";
 
 const About = () => {
   return (
     <div className="ui main container">
-      <h1 id="about-header" className="ui header">About Me</h1>
-      <p>
-        Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
-        corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum
-        molestias?
-      </p>
+    <div class="ui stackable two column grid">
+      <div class="column">
+        <UndrawArtificialIntelligence primaryColor='#12283a' height='200px' />
+      </div>
+      <div class="column">
+        <h1 className="ui header">About Me</h1>
+        <p>
+          Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
+          corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum
+          molestias?
+        </p>
+      </div>
     </div>
+  </div>
   );
 };
 
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import ProjectCard from "./ProjectCard"
+import { UndrawMobileApps } from "react-undraw-illustrations";
+
 
 
 class Projects extends Component {
@@ -33,7 +35,18 @@ class Projects extends Component {
   
       return (
         <div className="ui main container">
-          <h1 id="projects-header" className="ui header">My Projects</h1>
+          <div className="ui stackable two column grid">
+             <div className="column">
+               <UndrawMobileApps primaryColor='#12283a' height='200px'/>
+             </div>
+          
+          <div className="column">
+            <h1 id="projects-header" className="ui header">My Projects</h1>
+             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia quod
+          ab doloremque eaque. Consequatur temporibus, quos enim, eaque nemo ad
+          iusto sequi modi totam qui veniam? Ab asperiores inventore distinctio.</p>
+          </div>
+          </div>
           <div className="ui stackable four column grid">{projectsList}</div>
         </div>
       );
