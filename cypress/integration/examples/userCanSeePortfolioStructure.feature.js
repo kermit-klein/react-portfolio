b13d8b8cf8e56ffd756c2e550c4fc9fb7d80646e
allezz <aerbay@gmail.com>
2020-04-18 14:36:14 +0200

    Add button test

--- cypress/integration/examples/userCanSeePortfolioStructure.feature.js
+++ cypress/integration/examples/userCanSeePortfolioStructure.feature.js
@@ -1,8 +1,10 @@
 describe('Portfolio interface', () => {
     it('successfully renders',() => {
-      cy.visit('http://localhost:3000');
-      cy.get('#header').should('contain', 'My Portfolio');
-      cy.get('#footer').should('contain', 'Made with React 16.13.1');
-      cy.get('.github').should('contain', 'Github');
+      cy.visit('http://localhost:3000')
+      cy.get('#header').should('contain', 'My Portfolio')
+      cy.get('#footer').should('contain', 'Made with React 16.13.1')
+      cy.get('.github').should('contain', 'Github')
+      cy.get('.discogs').should('contain', 'Discogs')
+      cy.get('.linkedin').should('contain', 'LinkedIn')
     })
   })
