3a5be12eb1c39031546489efb4d62365c0c1f1fe
allezz <aerbay@gmail.com>
2020-04-19 11:43:04 +0200

    Trying to fix netlify

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -36,7 +36,7 @@ const encode = (data) => {
       return (
           <div className="ui container contact-container">
 
-                    <form className="ui form" onSubmit={this.handleSubmit} data-netlify="true" name="contact" action="POST">
+                    <form className="ui form" onSubmit={this.handleSubmit} data-netlify="true" name="contact" method="POST" action="POST">
                     <div className="field">
                         <label>Your Name</label>
                         <input type="text" name="name" value={name} onChange={this.handleChange} placeholder="Joe Doe"/>
