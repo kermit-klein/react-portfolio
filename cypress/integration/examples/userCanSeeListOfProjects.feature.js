24c501ea9f0bae1379e63b445d32b02d0ad672b4
allezz <aerbay@gmail.com>
2020-07-04 01:09:11 +0200

    Fix test files

--- cypress/integration/examples/userCanSeeListOfProjects.feature.js
+++ cypress/integration/examples/userCanSeeListOfProjects.feature.js
@@ -1,33 +1,42 @@
-describe('User can see list of projects', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3000')
-      cy.get('#projects-tab').click()
-    })
-  
-    it('displays first project and visit link', () => {
-      cy.get('#project-1').within(() => {
-        cy.get(('.image')).should('exist')
-        cy.get('.ui.header').should('contain', 'Library')
-        cy.get('.description').should('contain', 'A virtual library living in Ruby,It has 32 books which you can checkout and return but never read.')
-        cy.get('.ui.header').click()
-      })
-    })
-  
-    it('displays third project and visit link', () => {
-      cy.get('#project-3').within(() => {
-        cy.get(('.image')).should('exist')
-        cy.get('.ui.header').should('contain', 'FizzBuzz')
-        cy.get('.description').should('contain', "First thing I've done in Ruby, so be nice.")
-        cy.get('.ui.header').click()
-      })
-    })
-  
-    it('displays fifth project and visit link', () => {
-      cy.get('#project-5').within(() => {
-        cy.get(('.image')).should('exist')
-        cy.get('.ui.header').should('contain', 'Kermit Homepage')
-        cy.get('.description').should('contain', "You can learn about Kermit and purchase merchandise (at least you can try to).")
-        cy.get('.ui.header').click()
-      })
-    })  
-  })
+describe("User can see list of projects", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+    cy.get("#projects-tab").click();
+  });
+
+  it("displays first project and visit link", () => {
+    cy.get("#project-1").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".ui.header").should("contain", "YelpRecord");
+      cy.get(".description").should(
+        "contain",
+        "An app where you can create posts about your local record shops,rate them, comment on them. NodeJS/Express/MongoDB"
+      );
+      cy.get(".ui.header").click();
+    });
+  });
+
+  it("displays third project and visit link", () => {
+    cy.get("#project-3").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".ui.header").should("contain", "Daily News Sense Client");
+      cy.get(".description").should(
+        "contain",
+        "Client Side of a news web app. Geolocation, UI language selection, Stripe payment features are used in this project. Rails/React/PostgreSQL"
+      );
+      cy.get(".ui.header").click();
+    });
+  });
+
+  it("displays fifth project and visit link", () => {
+    cy.get("#project-5").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".ui.header").should("contain", "Cooper Test");
+      cy.get(".description").should(
+        "contain",
+        "A cooper test tracker for your own performance working with Rails API backend, Chart.js is used for drawing up nice performance graphs. Rails/React/PostgreSQL"
+      );
+      cy.get(".ui.header").click();
+    });
+  });
+});
