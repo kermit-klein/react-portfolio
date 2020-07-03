b1e39dba24c6f6801eb179cae682bb8f88605bbd
allezz <aerbay@gmail.com>
2020-07-03 20:52:11 +0200

    Edit css and change pictures

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,26 +1,27 @@
-import React from "react"
+import React from "react";
 // import { Link } from "react-router-dom"
 
 const ProjectCard = ({ project }) => {
   return (
     <>
-    <div className="ui link cards">
-      <a href={project.link}>
-      <div className="ui card">
-                
-        <div className="image">
-          <img className="prjcard" src={project.image}/>
-        </div>
-                  
-        <div className="content">
-          <h3 className="ui header"><a href={project.link}>{project.name}</a></h3>
-          <div className="description">{project.description}</div>
-        </div>
+      <div className="ui link cards">
+        <a href={project.link}>
+          <div className="ui card">
+            <div className="image">
+              <img className="prjcard" src={project.image} />
+            </div>
+
+            <div className="content">
+              <h3 className="ui header">
+                <a href={project.link}>{project.name}</a>
+              </h3>
+              <div className="description">{project.description}</div>
+            </div>
+          </div>
+        </a>
       </div>
-      </a>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default ProjectCard;
