162d5ce3313de92d4575895db54387299a37fefd
allezz <aerbay@gmail.com>
2020-09-12 18:07:56 +0200

    Add blog grid

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+const divStyle = {
+  marginTop: 100,
+  width: "70%",
+};
+
 const Blog = () => {
-  return <div></div>;
+  return (
+    <div className="ui container" style={divStyle}>
+      <div
+        id="retainable-rss-embed"
+        data-rss="https://medium.com/feed/@aerbay"
+        data-maxcols="3"
+        data-layout="grid"
+        data-poststyle="external"
+        data-readmore="Read the rest"
+        data-buttonclass="ui blue button"
+        data-offset="-100"
+      ></div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/Header.jsx
+++ src/Header.jsx
@@ -47,6 +47,7 @@ const Header = () => {
             activeStyle={{ fontWeight: "bold" }}
             to="/blog"
           >
+            <div class="floating ui red label">new</div>
             Blog
           </NavLink>
         </div>
